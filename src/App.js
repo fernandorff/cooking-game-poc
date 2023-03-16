ad99e67054389eaaa5eb4b52b0405dfe8468521b
Fernando Rocha <101672271+fernandorff@users.noreply.github.com>
2023-03-16 19:19:16 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import { Poc1 } from "./screen/poc1.jsx";
+import { Poc2 } from "./screen/poc2.jsx";
 
 export default function App() {
   return (
@@ -18,10 +19,16 @@ export default function App() {
                 Poc 1
               </Link>
             </li>
+            <li className="nav-item">
+              <Link to="/poc2" className="nav-link text-light">
+                Poc 2
+              </Link>
+            </li>
           </ul>
         </nav>
         <Routes>
           <Route path="/poc1" element={<Poc1 />} />
+          <Route path="/poc2" element={<Poc2 />} />
         </Routes>
       </div>
     </BrowserRouter>
---
+++ src/screen/poc2.jsx
@@ -0,0 +1,136 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./poc2.css";
+import { playSound } from "../helpers/playSound";
+
+export function Poc2() {
+  const [handlePosition, setHandlePosition] = useState(0);
+  const [panPosition, setPanPosition] = useState("");
+  const [draggingHandle, setDraggingHandle] = useState(false);
+  const [assetPositions, setAssetPositions] = useState({
+    hitboxAStart: 0,
+    hitboxAEnd: 0,
+    hitboxBStart: 0,
+    hitboxBEnd: 0,
+    panHandleCenter: 0,
+    pathStart: 0,
+    pathEnd: 0,
+  });
+  const [lastPlayedHitbox, setLastPlayedHitbox] = useState(null);
+  const [score, setScore] = useState(0);
+  const panHandleRef = useRef(null);
+  const [previousTouchPosition, setPreviousTouchPosition] = useState(null);
+
+  useEffect(() => {
+    const currentPosition = assetPositions.panHandleCenter;
+
+    if (currentPosition >= assetPositions.hitboxAStart && currentPosition <= assetPositions.hitboxAEnd) {
+      setPanPosition("Hitbox A");
+      if (lastPlayedHitbox !== "Hitbox A") {
+        setTimeout(() => {
+          playSound("beep");
+        }, 100);
+        setScore((prevScore) => prevScore + 5);
+        setLastPlayedHitbox("Hitbox A");
+      }
+    } else if (currentPosition >= assetPositions.hitboxBStart && currentPosition <= assetPositions.hitboxBEnd) {
+      setPanPosition("Hitbox B");
+      if (lastPlayedHitbox !== "Hitbox B") {
+        setTimeout(() => {
+          playSound("beep");
+        }, 100);
+        setScore((prevScore) => prevScore + 5);
+        setLastPlayedHitbox("Hitbox B");
+      }
+    } else if (currentPosition >= assetPositions.pathStart && currentPosition <= assetPositions.pathEnd) {
+      setPanPosition("Path");
+    } else {
+      setPanPosition("None");
+    }
+  }, [assetPositions, lastPlayedHitbox]);
+
+  useEffect(() => {
+    const hitboxA = document.querySelector(".hitbox-a");
+    const hitboxB = document.querySelector(".hitbox-b");
+    const path = document.querySelector(".path");
+
+    setAssetPositions((prevState) => ({
+      ...prevState,
+      hitboxAStart: hitboxA.getBoundingClientRect().left,
+      hitboxAEnd: hitboxA.getBoundingClientRect().right,
+      hitboxBStart: hitboxB.getBoundingClientRect().left,
+      hitboxBEnd: hitboxB.getBoundingClientRect().right,
+      pathStart: path.getBoundingClientRect().left,
+      pathEnd: path.getBoundingClientRect().right,
+    }));
+  }, []);
+
+  useEffect(() => {
+    const panHandle = panHandleRef.current;
+    if (panHandle) {
+      const panHandleRect = panHandle.getBoundingClientRect();
+      const panHandleCenter = (panHandleRect.left + panHandleRect.right) / 2;
+      setAssetPositions((prevState) => ({ ...prevState, panHandleCenter }));
+    }
+  }, [handlePosition]);
+
+  const handleMouseMove = (event) => {
+    if (draggingHandle) {
+      setHandlePosition((prevPosition) => prevPosition + event.movementX);
+    }
+  };
+
+  const handleTouchMove = (event) => {
+    if (draggingHandle) {
+      event.preventDefault();
+      const touch = event.targetTouches[0];
+
+      if (previousTouchPosition !== null) {
+        const touchMovement = touch.clientX - previousTouchPosition;
+        setHandlePosition((prevPosition) => prevPosition + touchMovement);
+      }
+
+      setPreviousTouchPosition(touch.clientX);
+    }
+  };
+
+  const StartDragPan = () => {
+    setDraggingHandle(true);
+  };
+
+  const EndDragPan = () => {
+    setDraggingHandle(false);
+    setPreviousTouchPosition(null);
+  };
+
+  return (
+    <div
+      className="poc1"
+      onMouseMove={handleMouseMove}
+      onMouseUp={EndDragPan}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={EndDragPan}
+      onTouchStart={StartDragPan}
+      style={{ touchAction: "none" }}
+    >
+      <progress className="progress" value={score} max={200} />
+      <div className="tomato" />
+
+      <div
+        className="knife"
+        ref={panHandleRef}
+        style={{ transform: `translateY(${handlePosition}px)` }}
+        onMouseDown={StartDragPan}
+        onTouchStart={StartDragPan}
+      />
+      <div className="box">
+        <div className="hitbox-a" />
+        <div className="path" />
+        <div className="hitbox-b" />
+      </div>
+      <p className="paragraph">
+        Current X position of the center of the pan-handle: {assetPositions.panHandleCenter}px
+      </p>
+      <p className="paragraph">Panhandle center is within: {panPosition}</p>
+    </div>
+  );
+}
